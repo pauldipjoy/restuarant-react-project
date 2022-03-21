d238c0026ecdbaeefc44efebfc3138f122f1aef7
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-03-21 16:56:40 +0600

    Creating store and Connecting to React App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import MainComponent from './components/MainComponent.js';
+import myStore from './redux/store';
 
 function App() {
   return (
     <div className ="App" >
 
-  <BrowserRouter>
+      <Provider store={myStore} >
+
+        <BrowserRouter>
       
-    <MainComponent  />
-      
-  </BrowserRouter>
+          <MainComponent   />
+        
+        </BrowserRouter>
+
+      </Provider>
+
+  
     
     </div>
   );
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -17,6 +17,7 @@ const initialState = {
 export const Reducer = (state = initialState , action) => {
 
     return state;
+    
 }
 
 
