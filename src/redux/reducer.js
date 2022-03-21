71c291eedc50f1bd57ec0693309b2ff384b2fd0f
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-03-21 15:34:04 +0600

    add Reducer for React app

---
+++ src/redux/reducer.js
@@ -0,0 +1,22 @@
+import COMMENTS from '../data/comments';
+import DISHES from '../data/dishes';
+
+
+
+
+
+const initialState = {
+
+    dishes : DISHES ,
+    comments : COMMENTS 
+
+}
+
+
+
+export const Reducer = (state = initialState , action) => {
+
+    return state;
+}
+
+
